60330004dd3117b38e1b75d6e3f80cefedc16819
Sula Michele <sulamichele95@gmail.com>
2023-05-01 21:17:45 +0200

    Complete, some bug fixing needed

---
+++ assets/scripts/main.js
@@ -0,0 +1,111 @@
+// Variables
+const ticketGenerator = document.querySelector("#ticket-generator");
+const clearValues = document.querySelector("#clear-values");
+const onlyLetters = /^[A-Za-z\s]+$/;
+const minCp = 10000;
+const maxCp = 99999;
+const minCarriage = 1;
+const maxCarriage = 12;
+
+// Function to generate train ticket
+ticketGenerator.addEventListener("click",
+    function createTicket() {
+
+        // Variables
+        let userName = document.getElementById("username").value;
+        let age = document.getElementById("age").value;
+        let km = document.getElementById("km").value;
+        
+        
+        
+        // Price
+        price = km * 0.21;
+
+
+        // Discount for young (<18) and old (>=65) age
+        if (age == "young") {
+        price *= 0.8;
+        } else if (age == "old") {
+        price *= 0.6;
+        } 
+
+        // Price to fixed
+        price = price.toFixed(2);
+
+        // Generation of ticket and alert when wrong inputs are detected
+        if ((km != "" && km !== null) && (userName != "" && userName !== null)) {
+            document.getElementById("ticket").innerHTML = price + "€";
+            document.getElementById("nome").innerHTML = userName;
+          } else  {
+            alert("Non hai inserito correttamente i dati!");
+            document.getElementById("ticket").innerHTML = "";
+            document.getElementById("nome").innerHTML = "";
+          }
+        
+        // Letters checker for username input with error message toggle
+        if (userName.match(onlyLetters)) {
+            userName = userName;
+            document.getElementById("lettersHelp").classList.add('hidden-text');
+        } else {
+            // alert("In nome e cognome vanno inserite solo lettere!");
+            document.getElementById("lettersHelp").classList.remove('hidden-text');
+            document.getElementById("nome").innerHTML = "";
+            document.getElementById("ticket").innerHTML = "";
+        }
+
+        // Numbers checker for KM input with error message toggle
+        if (isNaN(km)) {
+            // alert("In chilometri vanno inseriti solo numeri!");
+            document.getElementById("numbersHelp").classList.remove('hidden-text');
+            document.getElementById("nome").innerHTML = "";
+            document.getElementById("ticket").innerHTML = "";
+            return false;
+        } else {
+            document.getElementById("numbersHelp").classList.add('hidden-text');
+        }
+
+        // CP code creating random number
+        cpRandomNumber = getRandomNumberCp(minCp, maxCp);
+
+        function getRandomNumberCp(minCp, maxCp) {
+            min = Math.ceil(minCp);
+            max = Math.floor(maxCp);
+            return Math.floor(Math.random() * (maxCp - minCp) + minCp);
+          }
+        
+          document.getElementById("codice-cp").innerHTML = cpRandomNumber;
+
+          // Creating random number for carriage
+        carriageRandomNumber = getRandomNumberCarriage(minCarriage, maxCarriage);
+
+        function getRandomNumberCarriage(minCarriage, maxCarriage) {
+            min = Math.ceil(minCarriage);
+            max = Math.floor(maxCarriage);
+            return Math.floor(Math.random() * (maxCarriage - minCarriage) + minCarriage);
+          }
+        
+          document.getElementById("numero-carrozza").innerHTML = carriageRandomNumber;
+          
+          
+    }
+);
+
+// Inputclear on-click
+clearValues.addEventListener("click",
+    function clearFields() {
+        let km = document.getElementById("km");
+        let userName = document.getElementById("username");
+        
+        if (km.value != "") {
+            km.value = "";
+        }
+
+        if (userName.value != "") {
+            userName.value = "";
+        }
+        
+        document.getElementById("ticket").innerHTML = "";
+        document.getElementById("nome").innerHTML = "";
+    }
+);
+
